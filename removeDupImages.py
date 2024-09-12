b113823cad2f47d83d5e983f352b0098be9c31ef
Alex S <insten490@gmail.com>
2024-09-12 15:34:13 -0500

    Removing dup images init commit

---
+++ removeDupImages.py
@@ -0,0 +1,27 @@
+# Util to remove duplicated images across subdirectories
+import hashlib
+import os
+
+def pathInput() -> str:
+    directory = input("Please provide full path to the directory: ")
+    if not os.path.isdir(directory):
+        raise NotADirectoryError(f"No such directory named {directory} exists")
+    else:
+        return directory
+
+def main():
+    directory = pathInput()
+    hashset=set()
+
+    for filename in os.listdir(directory):
+        path = os.path.join(directory, filename)
+        digest = hashlib.sha1(open(path, 'rb').read()).digest()
+        if digest not in hashset:
+            hashset.add(digest)
+        else:
+            print(f"Removing {filename}...")
+            os.remove(path)
+
+# prompt the user for the path
+if __name__ == '__main__':
+    main()
