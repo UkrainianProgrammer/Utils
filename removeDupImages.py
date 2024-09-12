d9c5e299be27229ad1df7a1b1ead43173eba1d59
Alex S <insten490@gmail.com>
2024-09-12 15:53:03 -0500

    updated to include nested dirs

--- removeDupImages.py
+++ removeDupImages.py
@@ -13,14 +13,15 @@ def main():
     directory = pathInput()
     hashset=set()
 
-    for filename in os.listdir(directory):
-        path = os.path.join(directory, filename)
-        digest = hashlib.sha1(open(path, 'rb').read()).digest()
-        if digest not in hashset:
-            hashset.add(digest)
-        else:
-            print(f"Removing {filename}...")
-            os.remove(path)
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            path = os.path.join(dirpath, filename)
+            digest = hashlib.sha1(open(path, 'rb').read()).digest()
+            if digest not in hashset:
+                hashset.add(digest)
+            else:
+                print(f"Removing {path}...")
+                os.remove(path)
 
 # prompt the user for the path
 if __name__ == '__main__':
